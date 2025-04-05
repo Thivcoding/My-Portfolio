3933718290788b92811f343ba2dba8b915e27d37
Thivcoding <vantiv21@gmail.com>
2025-04-05 13:18:37 +0700

    Portfolio commit

---
+++ public/index.js
@@ -0,0 +1,35 @@
+const navbar = document.getElementById("navbar");
+const navdrop = document.getElementById("drop");
+        window.onscroll = (e) =>{
+          const scrollValue = window.scrollY;
+          console.log(scrollValue);
+          const valuescroll = 150;
+          if(window.scrollY > valuescroll){
+            navbar.style.backgroundColor = "#374151";
+            navbar.style.transition = "0.5s"
+            navbar.style.padding = "0px 0px";
+            navdrop.style.transition = "0.5s"
+            navdrop.style.top = "96p";
+
+          }
+          else{
+            navbar.style.backgroundColor = "transparent";
+            navbar.style.transition = "0.5s"
+            navbar.style.padding = "2px 0px";
+          }
+        }
+        alert("I'm Thivcoding ! Wellcome To my portfolio 🥰🙏🏽");
+
+        $(document).ready(function() {
+          $("#open").click(function() {
+            $("#open").addClass("hidden");
+            $("#close").removeClass("hidden");
+            $("#drop").addClass("left-[0]");
+          });
+      
+          $("#close").click(function() {
+            $("#close").addClass("hidden");
+            $("#open").removeClass("hidden");
+            $("#drop").removeClass("left-[0]");
+          });
+        });
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "*.html",
+    "./public/**/*.{html,js}",
+
+  ],
+  theme: {
+    extend: {
+      // screens: {
+      //   'max-769': { max: '769px' }, 
+      // },
+    },
+  },
+  plugins: [],
+}
+
