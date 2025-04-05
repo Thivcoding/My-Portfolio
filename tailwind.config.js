9018b82238198ae2c1b9f22fddc1f602c9326985
Thivcoding <vantiv21@gmail.com>
2025-04-05 18:41:18 +0700

    Already-project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "*.html",
-    "./public/**/*.{html,js}",
-
+    "./src/**/*.{html,js}", 
+    "./public/**/*.js",  
   ],
   theme: {
     extend: {
-      // screens: {
-      //   'max-769': { max: '769px' }, 
-      // },
     },
   },
   plugins: [],
